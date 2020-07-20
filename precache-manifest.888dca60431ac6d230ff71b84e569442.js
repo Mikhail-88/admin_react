a909e7abebeeea29e93233ba63f60eba4d52ca10
Mikhail-88 <mikhail.sapuzhak@gmail.com>
2020-07-20 21:14:28 +0300

    Updates

---
+++ precache-manifest.888dca60431ac6d230ff71b84e569442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40890ffb07c84ff8958f8b295397682f",
+    "url": "/admin_react/index.html"
+  },
+  {
+    "revision": "812ad761286231ec90a4",
+    "url": "/admin_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f3588afe3d03cfb5d24",
+    "url": "/admin_react/static/js/2.933ef578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/admin_react/static/js/2.933ef578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812ad761286231ec90a4",
+    "url": "/admin_react/static/js/main.c8e8a9f8.chunk.js"
+  },
+  {
+    "revision": "66b4b1866f9692853adf",
+    "url": "/admin_react/static/js/runtime-main.3f55217b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/admin_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
